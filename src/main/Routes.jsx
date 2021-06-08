e278e56d3c0980bf418d09b7297548b7cbcbc124
Joao Pedro <jpcar200@gmail.com>
2021-06-07 21:44:35 -0300

    add route funcionarios

--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -3,10 +3,12 @@ import { Switch, Route, Redirect } from 'react-router'
 
 import Home from '../components/home/Home'
 import UserCrud from '../components/user/UserCrud'
+import Funcionarios from '../components/funcionarios/Funcionarios'
 
 export default props =>
     <Switch>
         <Route exact path='/' component={Home} />
-        <Route path='/users' component={UserCrud} / >
+        <Route path='/users' component={UserCrud} />
+        <Route path='/Funcionarios' component={Funcionarios} />
         <Redirect from='*' to='/' />
     </Switch>
