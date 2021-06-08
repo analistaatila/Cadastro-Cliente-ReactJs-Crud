4b08debd1f93b43ad5f410101aabc040a749410f
Joao Pedro <jpcar200@gmail.com>
2021-06-07 21:46:49 -0300

    add import required bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './main/App';
 import registerServiceWorker from './registerServiceWorker';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-notifications/lib/notifications.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
